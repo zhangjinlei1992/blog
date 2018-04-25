04ee6e5acf6e608d8b8f31dc3f68ed5a6345abeb
zhangjinlei <zhangjinlei@yunbix.com>
2018-04-25 15:57:18 +0800

    newLoading

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 ;(function (window, undefined) {
     "use strict";
-
-
+    var welcomeWrap = document.getElementById('welcomeWrap');
+    welcomeWrap.classList.remove('loadingActive')
 })(window, undefined);
